340fc6f5572d1223309c96141aa9c7d4b9cbbce5
puneethirvichetty <puneeth2011@gmail.com>
2020-03-18 12:14:17 +0000

    commit

--- main.tf
+++ main.tf
@@ -1,11 +1,40 @@
 provider "google" {
-  credentials = file("account.json")
+  #credentials = file("account.json")
   project     = "cloudglobaldelivery-1000135575"
   region      = "us-central1"
 }
 
 resource "google_compute_instance" "default1" {
   name         = "test-a1"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+}
+}
+
+resource "google_compute_instance" "default" {
+  name         = "test-a"
   machine_type = "n1-standard-1"
   zone         = "us-central1-a"
 
