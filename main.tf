bfd19526f69f8316c39a836a526991445fb7cd83
Puneeth Irvichetty <puneeth2011@gmail.com>
2020-03-18 07:53:29 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "google" {
+  credentials = file("account.json")
+  project     = "cloudglobaldelivery-1000135575 "
+  region      = "us-central1"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "test-a"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+}
+}
